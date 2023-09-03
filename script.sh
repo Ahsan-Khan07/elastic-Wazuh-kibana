670466e1c71f74e8b9bec76bc8ecbbbba95d649a
Ahsan-Khan07 <qavitechnology@gmail.com>
2023-09-03 15:18:36 +0500

    optimized

--- script.sh
+++ script.sh
@@ -1,38 +1,22 @@
 # # !/bin/bash  
 
 # # Install the Wazuh Kibana plugin
-# /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/4.x/ui/kibana/wazuh_kibana-4.5.1_7.17.11-1.zip
+# /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/4.x/ui/kibana/wazuh_kibana-4.5.1_7.17.11-1.zip &&
 
 # # List installed Kibana plugins
-# /usr/share/kibana/bin/kibana-plugin list
-# user:root:root
+# /usr/share/kibana/bin/kibana-plugin list &&
+
+# /usr/share/kibana/bin/kibana-plugin remove wazuh &&
+
 # # Change ownership of Kibana data and plugins directories
-# chown -R kibana:kibana /usr/share/kibana/data
-# chown -R kibana:kibana /usr/share/kibana/plugins
+# chown -R kibana:kibana /usr/share/kibana/data &&
+# chown -R kibana:kibana /usr/share/kibana/plugins &&
 
 # # Install the Wazuh Kibana plugin again (if needed)
-# /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/4.x/ui/kibana/wazuh_kibana-4.5.1_7.17.11-1.zip
+# /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/4.x/ui/kibana/wazuh_kibana-4.5.1_7.17.11-1.zip &&
 
 # # List installed Kibana plugins again (if needed)
-# /usr/share/kibana/bin/kibana-plugin list
+# /usr/share/kibana/bin/kibana-plugin list &&
 
 # # Start Kibana Docker container
-# /usr/local/bin/kibana-docker
-
-
-
-
-# # #!/bin/bash
-
-# # # Change ownership of Kibana data and plugins directories
-# # chown -R kibana:kibana /usr/share/kibana/data
-# # chown -R kibana:kibana /usr/share/kibana/plugins
-
-# # # Install the Wazuh Kibana plugin
-# # /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/4.x/ui/kibana/wazuh_kibana-4.5.1_7.17.11-1.zip
-
-# # # List installed Kibana plugins
-# # /usr/share/kibana/bin/kibana-plugin list
-
-# # # Start Kibana Docker container
-# # /usr/local/bin/kibana-docker
+#  /usr/local/bin/kibana-docker --allow-root"
